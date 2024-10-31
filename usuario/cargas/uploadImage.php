66866b84b8b5f0fb4977cd8fa45fb785c30300a9
Inperiuz <marlonudenar@gmail.com>
2024-10-31 03:54:37 -0500

    feat: upload image

--- usuario/cargas/uploadImage.php
+++ usuario/cargas/uploadImage.php
@@ -12,7 +12,7 @@ if (isset($_FILES["image"]) && $_FILES["image"]["error"] == 0) {
     $newImgContent = file_get_contents($newImage);
 
     // Update the image data in the database
-    $sql = "UPDATE datosusuario SET image = ? WHERE email = ?";
+    $sql = "UPDATE u284478885_main SET image = ? WHERE email = ?";
     $statement = $conex->prepare($sql);
     $statement->bind_param('ss', $newImgContent, $email);
     $result = $statement->execute();
