e733c07d55630a613d98dbc82b80e6c21eee3305
Bm <140139146+Za1ks@users.noreply.github.com>
2024-10-30 23:34:32 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php 
-    $conex = mysqli_connect("mysql.so-udenar-archlinux.com", "u284478885_admin", "Solutiong3", "u284478885_main");
+    $conex = mysqli_connect("auth-db1389.hstgr.io", "u284478885_admin", "Solutiong3", "u284478885_main");
     
     if ($conex -> connect_error) {
         die("pta m".$conex -> connect_error);
