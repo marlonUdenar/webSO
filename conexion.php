8aa420d36b4cc9849d8dd49092b996cc3d4abf81
Bm <140139146+Za1ks@users.noreply.github.com>
2024-10-30 23:44:01 -0500

    final Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php 
-    $conex = mysqli_connect("auth-db1389.hstgr.io", "u284478885_admin", "Solutiong3", "u284478885_main");
-    
+    $conex = mysqli_connect("localhost", "u284478885_admin", "Solutiong3", "u284478885_main");
+    /*
     if ($conex -> connect_error) {
         die("pta m".$conex -> connect_error);
     }
     echo '<script>alert("nice")'
-
+    */
 ?>
 
