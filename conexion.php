e865cbf1261c29ecedd2c9ed726adcbf93602ac7
Brandon Moya <moyabec@gmail.com>
2024-10-30 23:27:06 -0500

    feat:conexion2

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php 
     $conex = mysqli_connect("localhost", "u284478885_admin", "Solutiong3", "u284478885_main");
-    /*
+    
     if ($conex -> connect_error) {
         die("pta m".$conex -> connect_error);
     }
     echo '<script>alert("nice")'
-    */
+
 ?>
 
