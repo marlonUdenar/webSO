50766dd473dba8aa4bbb6d9f1afd6e288cee268e
Brandon Moya <moyabec@gmail.com>
2024-11-01 11:58:33 -0500

    si fix

--- contact.php
+++ contact.php
@@ -1,4 +1,4 @@
-<?php require('./layouts/header.php') ?> 
+<?php require('layouts/headerU.php') ?> 
 
 <script>
 const homeLink = document.getElementById("contact");
@@ -23,25 +23,25 @@ homeLink.classList.add("active");
 	<head>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-		<link rel="stylesheet" href="css/main.css" />
-		<noscript><link rel="stylesheet" href="css/noscript.css" /></noscript>
+		<link rel="stylesheet" href="../css/main.css" />
+		<noscript><link rel="stylesheet" href="../css/noscript.css" /></noscript>
 	</head>
 	<body class="is-preload">
 
 		<!-- Wrapper -->
-      <?php require('loadevent.php') ?> 
+      <?php require('loadeventU.php') ?> 
 
 		<!-- Scripts -->
-			<script src="js/jqueryE.min.js"></script>
-			<script src="js/jqueryE.poptrox.min.js"></script>
-			<script src="js/browser.min.js"></script>
-			<script src="js/breakpoints.min.js"></script>
-			<script src="js/util.js"></script>
-			<script src="js/main.js"></script>
+			<script src="../js/jqueryE.min.js"></script>
+			<script src="../js/jqueryE.poptrox.min.js"></script>
+			<script src="../js/browser.min.js"></script>
+			<script src="../js/breakpoints.min.js"></script>
+			<script src="../js/util.js"></script>
+			<script src="../js/main.js"></script>
 
          </body>
 </html>
       <!-- end section -->
       
       <!--footer-->
-<?php require('./layouts/footer.php') ?>
+<?php require('layouts/footerU.php') ?>
